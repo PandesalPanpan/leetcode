2bf215f2175eb5267f1bbf4b33beb0169f6561ea
PandesalPanpan <petermarticio@gmail.com>
2025-09-26 15:00:53 +0800

    392: Final Solutions beats 100%

--- src/LeetCode75/392. Is Subsequence.js
+++ src/LeetCode75/392. Is Subsequence.js
@@ -3,7 +3,16 @@
  * @param {string} t
  * @return {boolean}
  */
-export default isSubsequence = function(s, t) {
-    if (s =='abc') return true;
-    if (s == 'axc') return false;
+export default isSubsequence = function (s, t) {
+    if (s == '' && t) return true;
+    let pointer = 0;
+    const sequence = t.split('');
+
+    sequence.forEach((char) => {
+        if (char == s[pointer]) {
+            pointer++;
+        }
+    })
+
+    return pointer == s.length;
 };
--- src/LeetCode75/392. Is Subsequence.test.js
+++ src/LeetCode75/392. Is Subsequence.test.js
@@ -4,6 +4,10 @@ it('abc is a subsequence of ahbgdc', () => {
     expect(isSubsequence('abc','ahbgdc')).toBe(true);
 })
 
-it('axc is a subsequence of ahbgdc', () => {
+it('axc is not a subsequence of ahbgdc', () => {
     expect(isSubsequence('axc','ahbgdc')).toBe(false);
+})
+
+it('empty s is a subsequence of ahbgdc', () => {
+    expect(isSubsequence('','ahbgdc')).toBe(true);
 })
